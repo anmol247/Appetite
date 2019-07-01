caf63f285f112dd82952c9f887fe6240bfcf58d8
anmol247 <anmoltiwari11@gmail.com>
2019-07-01 23:56:14 +0530

    viewcontroller added

---
+++ Appetite App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Appetite
+//
+//  Created by Anmol Tiwari on 29/06/19.
+//  Copyright © 2019 Anmol Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
